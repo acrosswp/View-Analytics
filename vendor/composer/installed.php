7c2be41157a241e8e5f2aabcc4f6703dd0764821
raftaar1191 <raftaar1191@gmail.com>
2023-11-09 13:38:57 +0530

    Update composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,45 @@
+<?php return array(
+    'root' => array(
+        'name' => 'acrosswp/view-analytics',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '634a265f170f242dd2f56f60cfcfcc24f3ea4283',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'acrosswp/acrosswp-plugin-update-checker-github' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8df5554adf40b5d5f54787b5315fbeee1b715ed7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../acrosswp/acrosswp-plugin-update-checker-github',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'acrosswp/view-analytics' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '634a265f170f242dd2f56f60cfcfcc24f3ea4283',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'yahnis-elsts/plugin-update-checker' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '00774ef3d9a2a3dfd8ac040b0dcdf5e42126a708',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../yahnis-elsts/plugin-update-checker',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
