f5d8c97c29a1b1b0149488aea6a38504fd5d7610
raftaar1191 <raftaar1191@gmail.com>
2023-12-15 15:27:31 +0530

    Update hook name

--- includes/media/view-analytics-counts.php
+++ includes/media/view-analytics-counts.php
@@ -274,7 +274,7 @@ class View_Analytics_Public_Media_Count {
 				/**
 				 * Fire a hook when someone view media for the first time
 				 */
-				do_action( $this->common->create_hooks_key( '_view_media' ), $id, $key_id, $current_user_id, $media_owner_id, 0, 1 );
+				do_action( $this->common->create_hooks_key( 'view_media' ), $id, $key_id, $current_user_id, $media_owner_id, 0, 1 );
 
 			} else {
 				$id = $views['id'];
@@ -284,7 +284,7 @@ class View_Analytics_Public_Media_Count {
 				 */
 				$old_ref_count = empty( $views['ref_count'] ) ? 1 : absint( $views['ref_count'] );
 				$ref_count = $old_ref_count + 1;
-				do_action( $this->common->create_hooks_key( '_view_media' ), $id, $key_id, $current_user_id, $views['author_id'], $old_ref_count, $ref_count );
+				do_action( $this->common->create_hooks_key( 'view_media' ), $id, $key_id, $current_user_id, $views['author_id'], $old_ref_count, $ref_count );
 
 				/**
 				 * Users list
@@ -305,7 +305,7 @@ class View_Analytics_Public_Media_Count {
 				$session_count		= $this->common->table->user_get( $current_user_id, $key_id, true );
 				$old_session_count	= absint( $views['session_count'] );
 				$session_count		= empty( $session_count ) ? $old_session_count + 1 : $old_session_count;
-				do_action( $this->common->create_hooks_key( '_sessions_view_media' ), $id, $key_id, $current_user_id, $views['author_id'], $old_session_count, $session_count );
+				do_action( $this->common->create_hooks_key( 'sessions_view_media' ), $id, $key_id, $current_user_id, $views['author_id'], $old_session_count, $session_count );
 
 				$this->common->table->user_update( 
 					$id,
